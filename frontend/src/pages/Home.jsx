027c18761a378656c45f96824d1bb90e8f47d07d
Randima Dias <randimamethminid525@gmail.com>
2024-09-08 19:24:08 +0530

    feat: implement the TopDoctors component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
+import SpecialityMenu from "../components/SpecialityMenu";
+import TopDoctors from "../components/TopDoctors";
 
 function home() {
   return (
     <div>
       <Header />
+      <SpecialityMenu />
+      <TopDoctors />
     </div>
   );
 }
